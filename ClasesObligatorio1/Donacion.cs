a99a8de2ae4ee3004bc26a4ad2b0ebb82a8f479b
vicente cersosimo sanz <vicenteonpoint@outlook.es>
2020-05-21 09:44:31 -0300

    Cambio en Obligatorio2 con MVC con todos los archivos

---
+++ ClasesObligatorio1/Donacion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio2MVC
+{
+    abstract class Donacion
+    {
+        private DateTime fechaDonacion;
+        private double vale;
+        private int codigoDonacion;
+        //habia creado un codigo para poder poder hacer el 
+        //alta de las Donaciones (TipoProducto y TipoEconomica)
+
+
+        public DateTime FechaDonacion
+        {
+            get { return fechaDonacion; }
+        }
+
+        public int CodigoDonacion
+        {
+            get { return codigoDonacion; }
+        }
+
+        public double Vale
+        {
+          get { return vale; }
+        }
+        
+        // no deberia estar no? es abstracta
+        /*public Donacion(DateTime fechaDonacion, int codigoDonacion)
+        {
+            this.fechaDonacion= fechaDonacion;
+            this.codigoDonacion = codigoDonacion;
+
+        }*/
+
+
+
+
+
+
+    }
+}
---
+++ Obligatorio2MVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Obligatorio2MVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Obligatorio2MVC")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en false, los componentes COM no verán los 
+// tipos de este ensamblado. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("0ff2396d-4fa9-4e58-8a0c-e1cc9344d711")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
